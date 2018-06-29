43b42678cbd5c6df3ccc670b8e9d6faf6e6b7e71
Eugene Tan <eugene.tan@ufinity.com>
2018-06-29 17:43:01 +0800

    My First SpringBoot API

---
+++ course-api-data/src/main/java/springbootstarter/course/CourseController.java
@@ -0,0 +1,48 @@
+package springbootstarter.course;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import springbootstarter.course.CourseService;
+import springbootstarter.topic.Topic;
+
+@RestController
+public class CourseController {
+
+	@Autowired
+	private CourseService courseService; 
+	
+	@RequestMapping(value = "/topics/{id}/courses", method = RequestMethod.GET)
+	public List<Course> getAllTopics(@PathVariable String id){
+		return courseService.getAllCourse(id);
+	}
+	
+	@RequestMapping(value = "/topics/{topicId}/courses/{id}", method = RequestMethod.GET)
+	public Optional<Course> getCourse(@PathVariable String id) {
+		return courseService.getCourse(id);
+	}
+	
+	@RequestMapping(value = "/topics/{topicId}/courses", method = RequestMethod.POST)
+	public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+		course.setTopic(new Topic(topicId, "", ""));
+		courseService.addCourse(course);
+	}
+	
+	@RequestMapping(value = "/topics/{topicId}/courses/{id}", method = RequestMethod.PUT)
+	public void updateCourse(@RequestBody Course course, @PathVariable String topicId, @PathVariable String id) {
+		course.setTopic(new Topic(topicId, "", ""));
+		courseService.updateCourse(course);
+	}
+	
+	@RequestMapping(value = "/topics/{topicId}/courses/{id}", method = RequestMethod.DELETE)
+	public void deleteCourse(@PathVariable String id) {
+		courseService.deleteCourse(id);
+	}
+}
